8a1b6d30b6a5aa0e3a82660599844904729aa6c9
gevangelista7 <gabrielevangelista7@poli.ufrj.br>
2022-05-02 14:59:42 -0300

    Some advances in the discipline.

--- Lista 3/src/Q3.py
+++ Lista 3/src/Q3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from test_functions import generate_rastrigin
 from simulated_annealing import simulated_annealing
 from fast_simulated_annealing import fast_sa
-
+import time
 
 if __name__ == "__main__":
     rd.seed(0)
@@ -21,21 +21,28 @@ if __name__ == "__main__":
     K_list = [8, 16, 32, 64]
     epsilon_list = [1e-1, 1e-2, 1e-3]
     T0_list = [1e-2, 1e-1, 1e1, 1e2]
+    general_start_time = time.time()
     for N in N_list:
         for K in K_list:
             for eps in epsilon_list:
                 for T0 in T0_list:
                     i += 1
+
+                    start_time_sa = time.time()
                     x_min_sa, J_min_sa, history_sa = simulated_annealing(calcJ=calcJ, x0=x0, N=N, K=K, epsilon=eps,
                                                                          T0=T0)
                     norm_x_sa = np.linalg.norm(x_min_sa)
-                    results[('SA', N, K, eps, T0)] = (J_min_sa, x_min_sa, norm_x_sa, history_sa)
+                    elapsed_time_sa = time.time() - start_time_sa
+                    results[('SA', N, K, eps, T0)] = (J_min_sa, x_min_sa, norm_x_sa, history_sa, elapsed_time_sa)
 
+                    start_time_fsa = time.time()
                     x_min_fsa, J_min_fsa, history_fsa = fast_sa(calcJ=calcJ, x0=x0, N=N, K=K, epsilon=eps, T0=T0)
                     norm_x_fsa = np.linalg.norm(x_min_sa)
-                    results[('FSA', N, K, eps, T0)] = (J_min_fsa, x_min_fsa, norm_x_fsa, history_fsa)
+                    elapsed_time_fsa = time.time() - start_time_fsa
+                    results[('FSA', N, K, eps, T0)] = (J_min_fsa, x_min_fsa, norm_x_fsa, history_fsa, elapsed_time_fsa)
 
-                    print("{} tentativas realizadas".format(i))
+
+                    print("{} tentativas realizadas, tempo total decorrido: {}".format(i, time.time() - general_start_time))
                     if norm_x_fsa < 0.3:
                         print("Candidata a solução válida achada pelo FSA!")
                         print("X_min = {} (norma= {}) \nJ_min = {}".format(x_min_fsa, norm_x_fsa, J_min_fsa))
